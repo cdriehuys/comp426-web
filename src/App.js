58b1aa4b6ddb2de49aaea3cc579138c1934cd6c9
Chathan Driehuys <chathan@driehuys.com>
2017-11-30 18:05:39 -0500

    Add basic home page.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import { Nav, NavItem, Navbar } from 'react-bootstrap';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+
+const App = () => (
+  <Navbar inverse collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>UltiManager</Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav pullRight>
+        <NavItem href="#">Log In</NavItem>
+        <NavItem href="#">Register</NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
 
 export default App;
