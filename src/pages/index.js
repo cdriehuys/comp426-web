9f973a34314226de7d723fb5bde9937076c86acf
Elijah Long <elijah498@gmail.com>
2017-12-08 22:00:05 -0500

    removed rosterpage last part.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
 export { default as HomePage } from './HomePage';
 export { default as LoginPage } from './LoginPage';
 export { default as RegistrationPage } from './RegistrationPage';
-export { default as RosterPage } from './RosterPage';
