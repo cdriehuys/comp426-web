cac643497866197a05a2ad2e64d001f94ac1a2d5
Elijah Long <elijah498@gmail.com>
2017-12-09 16:41:10 -0500

    add background to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Col, Image, Jumbotron, Row } from 'react-bootstrap';
+import { Col, Image, Jumbotron, Row, Tab, Tabs } from 'react-bootstrap';
 
 import image from '../images/frisbeePhoto.jpg';
-
+import paperBackground from '../images/paperBackground.jpg';
 
 const HomePage = () => (
-  <div>
+  <div style={{'backgroundImage':`url(${paperBackground})`,'borderRadius':'10px'}}>
     <Row>
       <Col sm={12} md={10} mdOffset={1} lg={8} lgOffset={2}>
-		    <Jumbotron className="text-center">
+		    <Jumbotron style={{boxShadow:'5px 10px 5px gray',}}className="text-center">
   		    <h1>Welcome!</h1>
   		    <p>UltiManager allows you to record statistics for a new game, or view the results of previous games.</p>
         </Jumbotron>
-        <Image responsive src={image} />
+        <Image style={{borderRadius:'10px'}}responsive src={image} />
+        <p>Image: Brian Whitter, 2017, CarolinaKickoff play, UNC at Chapel Hill Hooker Fields.</p>
       </Col>
     </Row>
   </div>
