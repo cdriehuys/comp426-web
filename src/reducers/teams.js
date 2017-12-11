d944896175675f7602fca450e2851dcd9a258bd9
Chathan Driehuys <chathan@driehuys.com>
2017-12-11 12:40:46 -0500

    Fix issue with team deletion not being respected.

--- src/reducers/teams.js
+++ src/reducers/teams.js
@@ -42,7 +42,6 @@ export default (state = {}, action = {}) => {
 
     case actions.TEAMS_FETCH_SUCCESS:
       return {
-        ...state,
         ...action.payload.teams.reduce((acc, team) => ({
           ...acc,
           [team.id]: {
